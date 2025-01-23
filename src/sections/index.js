8abf531b68bd6fa98660af0af3b1cfebab785e62
harshitcodex0 <harshit456987@hotmail.com>
2025-01-23 19:48:24 +0530

    .

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import Subscribe from './Subscribe';
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+
+export {
+    Hero,
+    PopularProducts,
+    Subscribe,
+    SuperQuality,
+    Services,
+    Footer,
+    SpecialOffer,
+    CustomerReviews
+}
